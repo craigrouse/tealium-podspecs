0e27e44298f37fdc7d8a222ec08bd80c0ed342ae
Merritt Tidwell <merritt.tidwell@gmail.com>
2016-11-07 11:42:34 -0800

    lifecycle podspec added

---
+++ TealiumIOSLifecycle/Example/Pods/Target Support Files/Pods-TealiumIOSLifecycle_Tests/Pods-TealiumIOSLifecycle_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TealiumIOSLifecycle_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TealiumIOSLifecycle_Tests
+@end
---
+++ TealiumIOSLifecycle/Example/Pods/Target Support Files/TealiumIOSLifecycle/TealiumIOSLifecycle-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double TealiumIOSLifecycleVersionNumber;
+FOUNDATION_EXPORT const unsigned char TealiumIOSLifecycleVersionString[];
+
